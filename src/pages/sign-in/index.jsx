4dbab2e57965f513e62e907734c1ca959632371a
@webdeveloper130390 <abdullokhpakhlavonov@gmail.com>
2024-09-20 09:04:20 +0500

    some-changes

--- src/pages/sign-in/index.jsx
+++ src/pages/sign-in/index.jsx
@@ -12,8 +12,8 @@ import Notification from '../utils/notification';
 import axios from 'axios';
 
 const Index = () => {
-  const [ form, setForm ] = useState({})
-  const [disabled, setDisabled] = useState(false )
+  const [form, setForm] = useState({})
+  const [disabled, setDisabled] = useState(false)
   const [count, setCount] = useState(0)
   const [time, setTime] = useState(10)
   const navigate = useNavigate()
@@ -30,22 +30,25 @@ const Index = () => {
       )
       .required("Password is required"),
   });
-  const handleSubmit = async(value) => {
+  const handleSubmit = async (value) => {
     console.log(value)
     try {
       const response = await axios.post("https://texnoark.ilyosbekdev.uz/auth/sign-in", value)
+      if(response.status == 201){
+        navigate('/admin')
+      }
     } catch (error) {
       console.log(error)
     }
-  } 
+  }
 
   return (
     <div>
       <div className="container mt-5">
-      <ToastContainer />
+        <ToastContainer />
         <div className="row">
           <div className="col-md-4 offset-3">
-            <div className="card" style={{width: "500px"}}>
+            <div className="card" style={{ width: "500px" }}>
               <div className="card-header">
                 <h1 className='text-center'>Sign In</h1>
               </div>
@@ -53,31 +56,31 @@ const Index = () => {
                 <Formik initialValues={initalValues} onSubmit={handleSubmit} validationSchema={signInValidationSchema}>
                   <Form id='sign-in'>
                     <Field
-                    name="phone_number"
-                    as={TextField}
-                    type="number"
-                    fullWidth
-                    margin="normal"
-                    variant="outlined"
-                    label="Phone number"
-                    helperText={
-                      <ErrorMessage name='phone_number' component="p" className='text-danger fs-6'/>
-                    }
+                      name="phone_number"
+                      as={TextField}
+                      type="text"
+                      fullWidth
+                      margin="normal"
+                      variant="outlined"
+                      label="Phone number"
+                      helperText={
+                        <ErrorMessage name='phone_number' component="p" className='text-danger fs-6' />
+                      }
                     />
-                    <Field 
-                    name="password"
-                    as={TextField}
-                    type="password"
-                    fullWidth
-                    variant="outlined"
-                    label="Password"
-                    helperText={
-                    <ErrorMessage 
-                      name='password'
-                      component="p"
-                      className='text-danger fs-6'
-                    />
-                    }
+                    <Field
+                      name="password"
+                      as={TextField}
+                      type="password"
+                      fullWidth
+                      variant="outlined"
+                      label="Password"
+                      helperText={
+                        <ErrorMessage
+                          name='password'
+                          component="p"
+                          className='text-danger fs-6'
+                        />
+                      }
                     />
                   </Form>
                 </Formik>
@@ -85,14 +88,14 @@ const Index = () => {
               </div>
               <div className="card-footer">
                 <center>
-              <Button variant='contained' style={{
-                width: "120px"
-              }} color='success' type='submit' form="sign-in">Save</Button>
-                </center>       
+                  <Button variant='contained' style={{
+                    width: "120px"
+                  }} color='success' type='submit' form="sign-in">Save</Button>
+                </center>
                 <div className="d-flex align-items-center justify-content-between gap-2">
-                <p className='mt-3'>Agar ro'yxatdan o'tmagan bo'lsangiz.</p>
-                <NavLink to="sign-up">Sign-Up</NavLink>
-                  </div>         
+                  <p className='mt-3'>Agar ro'yxatdan o'tmagan bo'lsangiz.</p>
+                  <NavLink to="sign-up">Sign-Up</NavLink>
+                </div>
               </div>
             </div>
           </div>
